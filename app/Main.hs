5c768602176ff667efade9148628f67654965448
Ashley Yakeley <ashley@semantic.org>
2022-12-16 21:44:19 -0800

    compare four candidates

--- app/Main.hs
+++ app/Main.hs
@@ -5,15 +5,39 @@ import Data.Int
 import Data.Word
 import Data.ByteString as BS
 import qualified Data.Serialize as Cereal
+import qualified Codec.Serialise as Serialise
+import qualified Data.Binary as Binary
+import qualified Codec.Winery as Winery
 
-class (Show a,Cereal.Serialize a) => Puttable a where
+class (
+        Show a,
+        Cereal.Serialize a,
+        Serialise.Serialise a,
+        Binary.Binary a,
+        Winery.Serialise a
+    ) => Puttable a where
     typeName :: String
+
+instance Puttable () where
+    typeName = "Unit"
+
+instance Puttable Bool where
+    typeName = "Bool"
 
 instance Puttable Int32 where
     typeName = "Int32"
 
 instance Puttable Word32 where
     typeName = "Word32"
+
+instance Puttable Integer where
+    typeName = "Integer"
+
+instance Puttable Float where
+    typeName = "Float"
+
+instance Puttable Double where
+    typeName = "Double"
 
 data TestItem = forall a. Puttable a => MkTestItem a
 
@@ -28,15 +52,28 @@ testCandidateItem (MkTestItem value) candidate = do
 
 candidates :: [Candidate]
 candidates =
-    [ MkCandidate "cereal" $ \val -> Cereal.runPut $ Cereal.put val
+    [ MkCandidate "cereal" $ Cereal.encode
+    , MkCandidate "serialise" $ BS.toStrict . Serialise.serialise
+    , MkCandidate "binary" $ BS.toStrict . Binary.encode
+    , MkCandidate "winery" $ Winery.serialise
     ]
 
 items :: [TestItem]
 items =
-    [ MkTestItem @Int32 34
+    [ MkTestItem @() ()
+    , MkTestItem @Bool False
+    , MkTestItem @Bool True
+    , MkTestItem @Int32 0
+    , MkTestItem @Integer 0
+    , MkTestItem @Float 0
+    , MkTestItem @Double 0
+    , MkTestItem @Int32 34
+    , MkTestItem @Integer 34
+    , MkTestItem @Float 34
+    , MkTestItem @Double 34
     ]
 
 main :: IO ()
 main = for_ items $ \item@(MkTestItem (value :: a)) -> do
-    putStrLn $ typeName @a <> " (" <> show value <> ")"
+    putStrLn $ typeName @a <> " " <> show value
     for_ candidates $ testCandidateItem item
