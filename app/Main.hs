38e7a4231bbd280b842fd06867b9c59ada8a6813
Ashley Yakeley <ashley@semantic.org>
2022-12-16 21:26:07 -0800

    generalise

--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,42 @@
 module Main (main) where
 
+import Data.Foldable
 import Data.Int
---import Data.Word
+import Data.Word
 import Data.ByteString as BS
 import qualified Data.Serialize as Cereal
 
-putCereal :: ByteString
-putCereal = Cereal.runPut $ do
-    Cereal.put (34 :: Int32)
+class (Show a,Cereal.Serialize a) => Puttable a where
+    typeName :: String
+
+instance Puttable Int32 where
+    typeName = "Int32"
+
+instance Puttable Word32 where
+    typeName = "Word32"
+
+data TestItem = forall a. Puttable a => MkTestItem a
+
+data Candidate = MkCandidate {
+    cName :: String,
+    cPut :: forall a. Puttable a => a -> ByteString
+}
+
+testCandidateItem :: TestItem -> Candidate -> IO ()
+testCandidateItem (MkTestItem value) candidate = do
+    putStrLn $ " " <> cName candidate <> ": " <> (show $ BS.length $ cPut candidate value)
+
+candidates :: [Candidate]
+candidates =
+    [ MkCandidate "cereal" $ \val -> Cereal.runPut $ Cereal.put val
+    ]
+
+items :: [TestItem]
+items =
+    [ MkTestItem @Int32 34
+    ]
 
 main :: IO ()
-main = do
-    putStrLn $ "cereal: " <> show (BS.length putCereal)
+main = for_ items $ \item@(MkTestItem (value :: a)) -> do
+    putStrLn $ typeName @a <> " (" <> show value <> ")"
+    for_ candidates $ testCandidateItem item
